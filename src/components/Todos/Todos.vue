3c40eca2e11e83e6afe10f0254c7fbb0aeed3a09
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2020-04-17 01:51:01 -0400

    Se crea la lista con todos

---
+++ src/components/Todos/Todos.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="Todos">
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <h3 >{{todo.title}}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Todos",
+        props:
+            ["todos"]
+        ,
+    };
+</script>
+
+<style>
+
+</style>
